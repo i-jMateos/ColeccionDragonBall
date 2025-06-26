4d6e7b6c6da0cad24ab00c609c3fdba798ad8b6d
Jordi <jordimatman@gmail.com>
2025-06-27 01:57:03 +0200

    Commit inicial net i funcional

---
+++ ColeccionDragonBall/ComicVolume.swift
@@ -0,0 +1,13 @@
+//
+//  ComicVolume.swift
+//  ColeccionDragonBall
+//
+//  Created by Jordi on 25/6/25.
+//
+import SwiftUI
+
+struct ComicVolume: Identifiable {
+    let id: Int
+    let title: String
+    let imageName: String
+}
---
+++ ColeccionDragonBallTests/ColeccionDragonBallTests.swift
@@ -0,0 +1,16 @@
+//
+//  ColeccionDragonBallTests.swift
+//  ColeccionDragonBallTests
+//
+//  Created by Jordi on 18/6/25.
+//
+
+import Testing
+
+struct ColeccionDragonBallTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
